6b43bc76a789ccf7ce574df906dbd1f50e3e62dc
Jeff Knox <knoxjeffrey@outlook.com>
2017-06-06 23:34:47 +0100

    setup for comments

--- web/models/topic.ex
+++ web/models/topic.ex
@@ -4,6 +4,7 @@ defmodule Discuss.Topic do
   schema "topics" do
     field :title, :string
     belongs_to :user, Discuss.User
+    has_many :comments, Discuss.Comment
   end
 
   # params defaults to an empty map with the \\ syntax
